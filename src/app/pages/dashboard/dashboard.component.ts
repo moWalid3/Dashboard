027925114f93b71a23e88df2180d23ca2874361d
Mohamed Walid <mohamedwalidtharwat2020@gmail.com>
2024-09-08 22:12:59 +0300

    add dashboard second-row

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,11 +3,13 @@ import { SalesStatisticsChartComponent } from "./sales-statistics-chart/sales-st
 import { TestChartsComponent } from "../../components/test-charts/test-charts.component";
 import { DashboardActivitiesComponent } from "./dashboard-activities/dashboard-activities.component";
 import { DashboardReportsAndSalesChartComponent } from "./dashboard-reports-and-sales-chart/dashboard-reports-and-sales-chart.component";
+import { DashboardTodoComponent } from "./dashboard-todo/dashboard-todo.component";
+import { DashboardMembersComponent } from "./dashboard-members/dashboard-members.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SalesStatisticsChartComponent, TestChartsComponent, DashboardActivitiesComponent, DashboardReportsAndSalesChartComponent],
+  imports: [SalesStatisticsChartComponent, TestChartsComponent, DashboardActivitiesComponent, DashboardReportsAndSalesChartComponent, DashboardTodoComponent, DashboardMembersComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
